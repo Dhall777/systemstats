92861df36bc76cbdac07b547d645785953985d63
root <darrian.hall12@gmail.com>
2023-03-30 06:42:03 +0000

    reworked mem generator logic | some work on cpu info generator | updated readme

--- lib/systemstats/cpu/cpuinfo/generator.ex
+++ lib/systemstats/cpu/cpuinfo/generator.ex
@@ -15,36 +15,18 @@ defmodule Systemstats.Cpu.Cpuinfo.Generator do
   end
 
   def handle_info(:work, state) do
-    spawn_link(&clean_insert_cpufreq_data/0)
+    spawn_link(&clean_insert_cpu_data/0)
     schedule_work()
     {:noreply, state}
   end
 
-  def clean_insert_cpufreq_data() do
-    # generate & handle incoming CPU info from /proc/cpuinfo -> to be stored/queried by Ecto to generate live plots, per 10 seconds
-    a_proc = System.cmd("cat", ["/proc/cpuinfo"]) |> Kernel.elem(0)
-    proc_a = String.slice(a_proc, 0..12)
-    proc_a_clean = String.replace(proc_a, "\t:", "")
-    # IO.inspect(proc_a_clean)
-    create_proc_a_atom = String.slice(proc_a_clean, 0..8) |> String.to_atom()
-    create_proc_a_int = String.slice(proc_a_clean, 10..11) |> String.to_integer()
+  def clean_insert_cpu_data() do
+    # (a) generate _ data from /proc/cpuinfo
 
-    a_freq = String.slice(a_proc, 155..173)
-    freq_a = String.replace(a_freq, "\t\t:", "")
-    freq_a_clean = String.replace(freq_a, "cpu MHz", "cpu_MHz")
-    # IO.inspect(freq_a_clean)
-    create_freq_a_atom = String.slice(freq_a_clean, 0..6) |> String.to_atom()
-    create_freq_a_float = String.slice(freq_a_clean, 8..16) |> String.to_float()
-
-    # Insert cleaned data -> this function is pulled from our context, alias it at some point...
+    # Insert cleaned cpu data
     Cpu.create_cpuinfo(%{
-      create_proc_a_atom => create_proc_a_int,
-      create_freq_a_atom => create_freq_a_float
+      # create_proc_a_atom => create_proc_a_int,
     })
-
-    # Debug code samples below
-    # debug = System.cmd("cat", ["/proc/cpuinfo"]) |> Kernel.elem(0)
-    # IO.inspect(debug)
   end
 
   def schedule_work() do
--- lib/systemstats/mem/meminfo/generator.ex
+++ lib/systemstats/mem/meminfo/generator.ex
@@ -9,6 +9,10 @@ defmodule Systemstats.Mem.Meminfo.Generator do
     GenServer.start_link(__MODULE__, %{})
   end
 
+  def schedule_work() do
+    Process.send_after(self(), :work, 10_000)
+  end
+
   def init(state) do
     schedule_work()
     {:ok, state}
@@ -21,7 +25,7 @@ defmodule Systemstats.Mem.Meminfo.Generator do
   end
 
   def clean_insert_memory_data() do
-    # (a) generate & insert MemFree data from /proc/meminfo
+    # (a) generate MemFree data from /proc/meminfo
     a_mem_raw = System.cmd("cat", ["/proc/meminfo"]) |> Kernel.elem(0) 
     a_mem_prep_a = String.replace(a_mem_raw, ~r(kB\n), "")
     a_mem_prep_b = String.replace(a_mem_prep_a, ~r(0\n), "")
@@ -32,7 +36,7 @@ defmodule Systemstats.Mem.Meminfo.Generator do
     create_mem_a_atom = String.slice(a_mem_prep_final, 0..6) |> String.to_atom()
     create_mem_a_int = String.slice(a_mem_prep_final, 7..700) |> String.to_integer()
 
-    # (b) generate & insert MemAvailable data from /proc/meminfo
+    # (b) generate MemAvailable data from /proc/meminfo
     b_mem_raw = System.cmd("cat", ["/proc/meminfo"]) |> Kernel.elem(0)
     b_mem_prep_a = String.replace(b_mem_raw, ~r(kB\n), "")
     b_mem_prep_b = String.replace(b_mem_prep_a, ~r(0\n), "")
@@ -43,7 +47,7 @@ defmodule Systemstats.Mem.Meminfo.Generator do
     create_mem_b_atom = String.slice(b_mem_prep_final, 0..11) |> String.to_atom()
     create_mem_b_int = String.slice(b_mem_prep_final, 12..120) |> String.to_integer()
 
-    # (c) generate & insert Buffers data from /proc/meminfo
+    # (c) generate Buffers data from /proc/meminfo
     c_mem_raw = System.cmd("cat", ["/proc/meminfo"]) |> Kernel.elem(0)
     c_mem_prep_a = String.replace(c_mem_raw, ~r(kB\n), "")
     c_mem_prep_b = String.replace(c_mem_prep_a, ~r(0\n), "")
@@ -54,7 +58,7 @@ defmodule Systemstats.Mem.Meminfo.Generator do
     create_mem_c_atom = String.slice(c_mem_prep_final, 0..6) |> String.to_atom()
     create_mem_c_int = String.slice(c_mem_prep_final, 7..700) |> String.to_integer()
 
-    # (d) generate & insert Cached data from /proc/meminfo
+    # (d) generate Cached data from /proc/meminfo
     d_mem_raw = System.cmd("cat", ["/proc/meminfo"]) |> Kernel.elem(0)
     d_mem_prep_a = String.replace(d_mem_raw, ~r(kB\n), "")
     d_mem_prep_b = String.replace(d_mem_prep_a, ~r(0\n), "")
@@ -65,7 +69,7 @@ defmodule Systemstats.Mem.Meminfo.Generator do
     create_mem_d_atom = String.slice(d_mem_prep_final, 0..5) |> String.to_atom()
     create_mem_d_int = String.slice(d_mem_prep_final, 6..600) |> String.to_integer()
 
-    # (e) generate & insert Active data from /proc/meminfo
+    # (e) generate Active data from /proc/meminfo
     e_mem_raw = System.cmd("cat", ["/proc/meminfo"]) |> Kernel.elem(0)
     e_mem_prep_a = String.replace(e_mem_raw, ~r(kB\n), "")
     e_mem_prep_b = String.replace(e_mem_prep_a, ~r(0\n), "")
@@ -76,7 +80,7 @@ defmodule Systemstats.Mem.Meminfo.Generator do
     create_mem_e_atom = String.slice(e_mem_prep_final, 0..5) |> String.to_atom()
     create_mem_e_int = String.slice(e_mem_prep_final, 6..600) |> String.to_integer()
 
-    # (f) generate & insert Dirty data from /proc/meminfo
+    # (f) generate Dirty data from /proc/meminfo
     f_mem_raw = System.cmd("cat", ["/proc/meminfo"]) |> Kernel.elem(0)
     f_mem_prep_a = String.replace(f_mem_raw, ~r(kB\n), "")
     f_mem_prep_b = String.replace(f_mem_prep_a, ~r(0\n), "")
@@ -87,7 +91,7 @@ defmodule Systemstats.Mem.Meminfo.Generator do
     create_mem_f_atom = String.slice(f_mem_prep_final, 0..4) |> String.to_atom()
     create_mem_f_int = String.slice(f_mem_prep_final, 5..500) |> String.to_integer()
 
-    # (g) generate & insert SwapFree data from /proc/meminfo
+    # (g) generate SwapFree data from /proc/meminfo
     g_mem_raw = System.cmd("cat", ["/proc/meminfo"]) |> Kernel.elem(0)
     g_mem_prep_a = String.replace(g_mem_raw, ~r(kB\n), "")
     g_mem_prep_b = String.replace(g_mem_prep_a, ~r(0\n), "")
@@ -98,7 +102,7 @@ defmodule Systemstats.Mem.Meminfo.Generator do
     create_mem_g_atom = String.slice(g_mem_prep_final, 0..7) |> String.to_atom()
     create_mem_g_int = String.slice(g_mem_prep_final, 8..800) |> String.to_integer()
 
-    # (h) generate & insert PageTables data from /proc/meminfo
+    # (h) generate PageTables data from /proc/meminfo
     h_mem_raw = System.cmd("cat", ["/proc/meminfo"]) |> Kernel.elem(0)
     h_mem_prep_a = String.replace(h_mem_raw, ~r(kB\n), "")
     h_mem_prep_b = String.replace(h_mem_prep_a, ~r(0\n), "")
@@ -109,7 +113,7 @@ defmodule Systemstats.Mem.Meminfo.Generator do
     create_mem_h_atom = String.slice(h_mem_prep_final, 0..9) |> String.to_atom()
     create_mem_h_int = String.slice(h_mem_prep_final, 10..1000) |> String.to_integer()
 
-    # (i) generate & insert Writeback data from /proc/meminfo
+    # (i) generate Writeback data from /proc/meminfo
     i_mem_raw = System.cmd("cat", ["/proc/meminfo"]) |> Kernel.elem(0)
     i_mem_prep_a = String.replace(i_mem_raw, ~r(kB\n), "")
     i_mem_prep_b = String.replace(i_mem_prep_a, ~r(0\n), "")
@@ -120,7 +124,7 @@ defmodule Systemstats.Mem.Meminfo.Generator do
     create_mem_i_atom = String.slice(i_mem_prep_final, 0..8) |> String.to_atom()
     create_mem_i_int = String.slice(i_mem_prep_final, 9..900) |> String.to_integer()
 
-    # (j) generate & insert NFS_Unstable data from /proc/meminfo
+    # (j) generate NFS_Unstable data from /proc/meminfo
     j_mem_raw = System.cmd("cat", ["/proc/meminfo"]) |> Kernel.elem(0)
     j_mem_prep_a = String.replace(j_mem_raw, ~r(kB\n), "")
     j_mem_prep_b = String.replace(j_mem_prep_a, ~r(0\n), "")
@@ -132,7 +136,7 @@ defmodule Systemstats.Mem.Meminfo.Generator do
     create_mem_j_atom = String.slice(j_mem_prep_final, 0..11) |> String.to_atom()
     create_mem_j_int = String.slice(j_mem_prep_final, 12..1200) |> String.to_integer()
 
-    # (k) generate & insert MemTotal data from /proc/meminfo
+    # (k) generate MemTotal data from /proc/meminfo
     k_mem_raw = System.cmd("cat", ["/proc/meminfo"]) |> Kernel.elem(0)
     k_mem_prep_a = String.replace(k_mem_raw, ~r(kB\n), "")
     k_mem_prep_b = String.replace(k_mem_prep_a, ~r(0\n), "")
@@ -143,7 +147,7 @@ defmodule Systemstats.Mem.Meminfo.Generator do
     create_mem_k_atom = String.slice(k_mem_prep_final, 0..9) |> String.to_atom()
     create_mem_k_int = String.slice(k_mem_prep_final, 10..1000) |> String.to_integer()
 
-    # (l) generate & insert SwapTotal data from /proc/meminfo
+    # (l) generate SwapTotal data from /proc/meminfo
     l_mem_raw = System.cmd("cat", ["/proc/meminfo"]) |> Kernel.elem(0)
     l_mem_prep_a = String.replace(l_mem_raw, ~r(kB\n), "")
     l_mem_prep_b = String.replace(l_mem_prep_a, ~r(0\n), "")
@@ -169,10 +173,6 @@ defmodule Systemstats.Mem.Meminfo.Generator do
       create_mem_k_atom => create_mem_k_int,
       create_mem_l_atom => create_mem_l_int,
     })
-  end
-
-  def schedule_work() do
-    Process.send_after(self(), :work, 10_000)
   end
 
 end
